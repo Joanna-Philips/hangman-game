614e576fa4e24a868b3e825b51c4681248c820e0
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-08-22 14:15:06 +0200

    Update HangmanDrawing.tsx
    Hangman drawing progress- done head, body, and arms

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -14,13 +14,40 @@ const HEAD = (
 const BODY = (
     <div 
     style={{
-        width: "50px",
-        height: "50px",
-        borderRadius: "100%",
-        border: "8px solid teal",
+        width: "8px",
+        height: "100px",
+        background: "teal",
         position: "absolute",
-        top: "45px",
-        right: "-28px"
+        top: "110px",
+        right: 0
+    }}/>
+)
+
+const RIGHT_ARM = (
+    <div 
+    style={{
+        width: "100px",
+        height: "8px",
+        background: "teal",
+        position: "absolute",
+        top: "150px",
+        right: "-100px",
+        rotate: "-30deg",
+        transformOrigin: "left bottom"
+    }}/>
+)
+
+const LEFT_ARM = (
+    <div 
+    style={{
+        width: "100px",
+        height: "8px",
+        background: "teal",
+        position: "absolute",
+        top: "150px",
+        right: "8px",
+        rotate: "30deg",
+        transformOrigin: "right bottom"
     }}/>
 )
 
@@ -29,6 +56,9 @@ export const HangmanDrawing = () => {
   return (
     <div style={{ position: "relative" }}>
         {HEAD}
+        {BODY}
+        {RIGHT_ARM}
+        {LEFT_ARM}
       <div
         style={{
           height: "50px",
